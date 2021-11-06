8b51f9a05dc9c56696ea0709558ebc8a4e90bec3
sheepfly <developer@noreply.email.com>
2021-11-07 04:49:51 +0800

    增加MybatisUtil,修改数据库字段

--- src/main/java/com/sheepfly/media/entity/Author.java
+++ src/main/java/com/sheepfly/media/entity/Author.java
@@ -19,18 +19,36 @@ public class Author implements Serializable {
 
     @TableId("ID")
     private String id;
-
-    @TableField("NAME")
-    private String name;
-
+    /**
+     * 用户名。
+     */
+    @TableField("USER_NAME")
+    private String userName;
+    /**
+     * 创建时间。
+     */
     @TableField("CREATE_TIME")
     private String createTime;
-
+    /**
+     * 更新时间。
+     */
     @TableField("UPDATE_TIME")
     private String updateTime;
-
+    /**
+     * 来源。
+     */
     @TableField("SOURCE")
     private String source;
+    /**
+     * 用户在网站注册的id，如果网站只显示用户名，则使用用户名。
+     */
+    @TableField("USER_ID")
+    private String userId;
+    /**
+     * 用户主页。
+     */
+    @TableField("USER_SPACE")
+    private String userSpace;
 
 
     public String getId() {
@@ -41,12 +59,12 @@ public class Author implements Serializable {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public String getCreateTime() {
@@ -73,14 +91,32 @@ public class Author implements Serializable {
         this.source = source;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserSpace() {
+        return userSpace;
+    }
+
+    public void setUserSpace(String userSpace) {
+        this.userSpace = userSpace;
+    }
+
     @Override
     public String toString() {
         return "Author{" +
-                "id=" + id +
-                ", name=" + name +
-                ", createTime=" + createTime +
-                ", updateTime=" + updateTime +
-                ", source=" + source +
-                "}";
+                "id='" + id + '\'' +
+                ", userName='" + userName + '\'' +
+                ", createTime='" + createTime + '\'' +
+                ", updateTime='" + updateTime + '\'' +
+                ", source='" + source + '\'' +
+                ", userId='" + userId + '\'' +
+                ", userSpace='" + userSpace + '\'' +
+                '}';
     }
 }
