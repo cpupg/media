4b7f566da759f1661343946abc042164741e1063
sheepfly <developer@noreply.email.com>
2021-12-07 23:06:47 +0800

    增加配置文件

--- src/main/java/com/sheepfly/media/MediaApplication.java
+++ src/main/java/com/sheepfly/media/MediaApplication.java
@@ -3,9 +3,11 @@ package com.sheepfly.media;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @MapperScan("com.sheepfly.media.mapper")
 @SpringBootApplication
+@ImportResource("classpath:configs/springboot.xml")
 public class MediaApplication {
 
     public static void main(String[] args) {
