945b2213afc30068806328abe9d9f3986ca67a5f
sheepfly <developer@noreply.email.com>
2022-02-27 22:25:46 +0800

    AuthorVo增加getter和setter

--- src/main/java/com/sheepfly/media/vo/AuthorVo.java
+++ src/main/java/com/sheepfly/media/vo/AuthorVo.java
@@ -43,4 +43,73 @@ public class AuthorVo implements Serializable {
      * 更新时间
      */
     private LocalDate updateTime;
+
+    @Override
+    public String toString() {
+        return "AuthorVo{" +
+                "id='" + id + '\'' +
+                ", userId='" + userId + '\'' +
+                ", username='" + username + '\'' +
+                ", site=" + site +
+                ", homepage='" + homepage + '\'' +
+                ", createTime=" + createTime +
+                ", updateTime=" + updateTime +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Site getSite() {
+        return site;
+    }
+
+    public void setSite(Site site) {
+        this.site = site;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public LocalDate getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDate createTime) {
+        this.createTime = createTime;
+    }
+
+    public LocalDate getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDate updateTime) {
+        this.updateTime = updateTime;
+    }
 }
