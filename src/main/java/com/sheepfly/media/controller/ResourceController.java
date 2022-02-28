c99601e052f26f15e4c2c325c64e3cc4161314eb
sheepfly <developer@noreply.email.com>
2022-02-28 22:55:29 +0800

    增加资源查询功能。

--- src/main/java/com/sheepfly/media/controller/ResourceController.java
+++ src/main/java/com/sheepfly/media/controller/ResourceController.java
@@ -1,7 +1,6 @@
 package com.sheepfly.media.controller;
 
 
-import com.sheepfly.media.entity.Resource;
 import com.sheepfly.media.service.IResourceService;
 import com.sheepfly.media.vo.ResourceVo;
 import com.sheepfly.media.vo.common.ProTableObject;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,13 +31,8 @@ public class ResourceController {
     @PostMapping("/queryResourceList")
     @ResponseBody
     public ProTableObject<ResourceVo> queryResourceList() {
-        ResourceVo resourceVo = new ResourceVo();
-        List<ResourceVo> list = new ArrayList<>();
-        list.add(resourceVo);
-        List<Resource> list1 = service.list(null);
-        System.out.println(list1.toString());
-        service.queryResourceVoList(null);
-        return new ProTableObject<>(list.size(), list);
+        List<ResourceVo> resourceVoList = service.queryResourceVoList(null);
+        return new ProTableObject<>(resourceVoList.size(), resourceVoList);
     }
 }
 
