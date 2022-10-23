4708f1f5b88793e8bd25c3f3437375ee85e52882
wrote-code <developer@noreply.email.com>
2022-10-23 13:41:25 +0800

    增加全局异常处理器。
    新增：
    1.增加全局异常处理器。
    
    依赖改动：
    1.增加mybatis generator依赖。
    
    其他改动：
    1.根目录增加idea代码检查配置文件

---
+++ src/main/java/com/sheepfly/media/exception/BusinessExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.sheepfly.media.exception;
+
+import com.sheepfly.media.vo.common.ErrorCode;
+import com.sheepfly.media.vo.common.ResponseData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 全局异常处理器。
+ *
+ * @author wrote-code
+ */
+@ControllerAdvice
+public class BusinessExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(BusinessExceptionHandler.class);
+
+    /**
+     * 处理捕捉到的业务异常。
+     *
+     * @param e 异常。
+     *
+     * @return 通用返回结果。
+     */
+    @ExceptionHandler(BusinessException.class)
+    @ResponseBody
+    public ResponseData<BusinessException> handleBusinessException(BusinessException e) {
+        log.error("发生业务异常", e);
+        return ResponseData.fail(e.getError());
+    }
+
+    /**
+     * 解析捕捉到的非业务异常（未知异常）。
+     *
+     * @param e 异常。
+     *
+     * @return 通用返回结果。
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseData<Exception> handleException(Exception e) {
+        log.error("发生未知异常", e);
+        return ResponseData.fail(ErrorCode.UNEXPECT_ERROR);
+    }
+}
