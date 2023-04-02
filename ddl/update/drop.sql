fe4475e70a833fb684e3de6540576656f3635db0
wrote-code <developer@noreply.email.com>
2023-04-03 00:54:02 +0800

    增加升级sql

---
+++ ddl/update/drop.sql
@@ -0,0 +1,7 @@
+drop table public.ALBUM;
+drop table public.AUTHOR;
+drop table public.RESOURCE;
+drop table public.RESOURCE_ALBUM;
+drop table public.RESOURCE_TYPE;
+drop table public.RESOURCE_TYPE_MAP;
+drop table public.SITE;
