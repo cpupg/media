70eb6b8384bf945f0ed951e0c75ec7e0d97bf82c
wrote-code <developer@noreply.email.com>
2023-04-25 01:25:52 +0800

    删除无用表，即对应的ddl。ddl建表时增加模式名。

--- ddl/init/media.sql
+++ ddl/init/media.sql
@@ -1,214 +1,123 @@
-create table ALBUM
+create table MEDIA.AUTHOR
 (
-  ID CHARACTER VARYING(19) not null
+  ID            CHARACTER VARYING(19) not null
     primary key,
-  ALBUM_NAME CHARACTER VARYING(90) not null,
-  AUTHOR_ID CHARACTER VARYING(19) not null,
-  CREATE_TIME TIMESTAMP not null,
-  UPDATE_TIME TIMESTAMP
+  USER_ID       CHARACTER VARYING(90),
+  USERNAME      CHARACTER VARYING(90) not null,
+  SITE_ID       CHARACTER VARYING(19) not null,
+  HOMEPAGE      CHARACTER VARYING(90),
+  CREATE_TIME   TIMESTAMP             not null,
+  UPDATE_TIME   TIMESTAMP,
+  DELETE_STATUS INTEGER default 0     not null
 );
 
-comment on table ALBUM is '专辑';
+comment on table MEDIA.AUTHOR is '创作人员';
 
-comment on column ALBUM.ID is 'ID';
+comment on column MEDIA.AUTHOR.ID is 'ID';
 
-comment on column ALBUM.ALBUM_NAME is '专辑';
+comment on column MEDIA.AUTHOR.USER_ID is '用户在站点注册时的id';
 
-comment on column ALBUM.AUTHOR_ID is '专辑作者';
+comment on column MEDIA.AUTHOR.USERNAME is '用户名';
 
-comment on column ALBUM.CREATE_TIME is '创建时间';
+comment on column MEDIA.AUTHOR.SITE_ID is '注册站点ID';
 
-comment on column ALBUM.UPDATE_TIME is '更新时间';
+comment on column MEDIA.AUTHOR.HOMEPAGE is '主页';
 
-create table AUTHOR
+comment on column MEDIA.AUTHOR.CREATE_TIME is '创建时间';
+
+comment on column MEDIA.AUTHOR.UPDATE_TIME is '更新时间';
+
+comment on column MEDIA.AUTHOR.DELETE_STATUS is '删除状态0未删除1删除';
+
+create index MEDIA.AUTHOR_1
+  on MEDIA.AUTHOR (USER_ID);
+
+create index MEDIA.AUTHOR_2
+  on MEDIA.AUTHOR (USERNAME);
+
+create index MEDIA.AUTHOR_3
+  on MEDIA.AUTHOR (SITE_ID);
+
+create index MEDIA.AUTHOR_4
+  on MEDIA.AUTHOR (CREATE_TIME);
+
+create index MEDIA.AUTHOR_5
+  on MEDIA.AUTHOR (UPDATE_TIME);
+
+create table MEDIA.RESOURCE
 (
-  ID CHARACTER VARYING(19) not null
+  ID            CHARACTER VARYING(19)  not null
     primary key,
-  USER_ID CHARACTER VARYING(90),
-  USERNAME CHARACTER VARYING(90) not null,
-  SITE_ID CHARACTER VARYING(19) not null,
-  HOMEPAGE CHARACTER VARYING(90),
-  CREATE_TIME TIMESTAMP not null,
-  UPDATE_TIME TIMESTAMP,
-  DELETE_STATUS INTEGER default 0 not null
+  FILENAME      CHARACTER VARYING(90)  not null,
+  DIR           CHARACTER VARYING(900) not null,
+  AUTHOR_ID     CHARACTER VARYING(19)  not null,
+  ALBUM_ID      CHARACTER VARYING(19),
+  CREATE_TIME   TIMESTAMP              not null,
+  UPDATE_TIME   TIMESTAMP,
+  SAVE_TIME     TIMESTAMP,
+  DELETE_STATUS INTEGER default 0      not null
 );
 
-comment on table AUTHOR is '创作人员';
+comment on table MEDIA.RESOURCE is '资源';
 
-comment on column AUTHOR.ID is 'ID';
+comment on column MEDIA.RESOURCE.ID is 'ID';
 
-comment on column AUTHOR.USER_ID is '用户在站点注册时的id';
+comment on column MEDIA.RESOURCE.FILENAME is '文件名';
 
-comment on column AUTHOR.USERNAME is '用户名';
+comment on column MEDIA.RESOURCE.DIR is '资源目录';
 
-comment on column AUTHOR.SITE_ID is '注册站点ID';
+comment on column MEDIA.RESOURCE.AUTHOR_ID is '作者id';
 
-comment on column AUTHOR.HOMEPAGE is '主页';
+comment on column MEDIA.RESOURCE.ALBUM_ID is '专辑id';
 
-comment on column AUTHOR.CREATE_TIME is '创建时间';
+comment on column MEDIA.RESOURCE.CREATE_TIME is '创建时间';
 
-comment on column AUTHOR.UPDATE_TIME is '更新时间';
+comment on column MEDIA.RESOURCE.UPDATE_TIME is '更新时间';
 
-comment on column AUTHOR.DELETE_STATUS is '删除状态0未删除1删除';
+comment on column MEDIA.RESOURCE.SAVE_TIME is '保存时间';
 
-create index AUTHOR_1
-  on AUTHOR (USER_ID);
+comment on column MEDIA.RESOURCE.DELETE_STATUS is '删除状态0未删除1删除';
 
-create index AUTHOR_2
-  on AUTHOR (USERNAME);
+create index MEDIA.RESOURCE_1
+  on MEDIA.RESOURCE (CREATE_TIME desc);
 
-create index AUTHOR_3
-  on AUTHOR (SITE_ID);
+create index MEDIA.RESOURCE_2
+  on MEDIA.RESOURCE (UPDATE_TIME desc);
 
-create index AUTHOR_4
-  on AUTHOR (CREATE_TIME);
+create index MEDIA.RESOURCE_3
+  on MEDIA.RESOURCE (FILENAME);
 
-create index AUTHOR_5
-  on AUTHOR (UPDATE_TIME);
-
-create table RESOURCE
+create table MEDIA.SITE
 (
-  ID CHARACTER VARYING(19) not null
+  ID            CHARACTER VARYING(19) not null
     primary key,
-  FILENAME CHARACTER VARYING(90) not null,
-  DIR CHARACTER VARYING(900) not null,
-  AUTHOR_ID CHARACTER VARYING(19) not null,
-  ALBUM_ID CHARACTER VARYING(19),
-  CREATE_TIME TIMESTAMP not null,
-  UPDATE_TIME TIMESTAMP,
-  SAVE_TIME TIMESTAMP,
-  DELETE_STATUS INTEGER default 0 not null
+  SITE_NAME     CHARACTER VARYING(90) not null,
+  URL           CHARACTER VARYING(90) not null,
+  CREATE_TIME   TIMESTAMP             not null,
+  UPDATE_TIME   TIMESTAMP,
+  DELETE_STATUS INTEGER default 0     not null
 );
 
-comment on table RESOURCE is '资源';
+comment on table MEDIA.SITE is '站点';
 
-comment on column RESOURCE.ID is 'ID';
+comment on column MEDIA.SITE.ID is 'ID';
 
-comment on column RESOURCE.FILENAME is '文件名';
+comment on column MEDIA.SITE.SITE_NAME is '网站名称';
 
-comment on column RESOURCE.DIR is '资源目录';
+comment on column MEDIA.SITE.URL is '网站地址';
 
-comment on column RESOURCE.AUTHOR_ID is '作者id';
+comment on column MEDIA.SITE.CREATE_TIME is '创建时间';
 
-comment on column RESOURCE.ALBUM_ID is '专辑id';
+comment on column MEDIA.SITE.UPDATE_TIME is '更细时间';
 
-comment on column RESOURCE.CREATE_TIME is '创建时间';
+comment on column MEDIA.SITE.DELETE_STATUS is '删除状态0未删除1删除';
 
-comment on column RESOURCE.UPDATE_TIME is '更新时间';
+create index MEDIA.SITE_1
+  on MEDIA.SITE (SITE_NAME);
 
-comment on column RESOURCE.SAVE_TIME is '保存时间';
+create index MEDIA.SITE_2
+  on MEDIA.SITE (CREATE_TIME);
 
-comment on column RESOURCE.DELETE_STATUS is '删除状态0未删除1删除';
-
-create index RESOURCE_1
-  on RESOURCE (CREATE_TIME desc);
-
-create index RESOURCE_2
-  on RESOURCE (UPDATE_TIME desc);
-
-create index RESOURCE_3
-  on RESOURCE (FILENAME);
-
-create table RESOURCE_ALBUM
-(
-  ID CHARACTER VARYING(19) not null
-    primary key,
-  CREATE_TIME TIMESTAMP not null,
-  UPDATE_TIME TIMESTAMP,
-  RESOURCE_ID CHARACTER VARYING(19) not null,
-  ALBUM_ID CHARACTER VARYING(19) not null
-);
-
-comment on table RESOURCE_ALBUM is '资源所属专辑';
-
-comment on column RESOURCE_ALBUM.ID is 'ID';
-
-comment on column RESOURCE_ALBUM.CREATE_TIME is '创建时间';
-
-comment on column RESOURCE_ALBUM.UPDATE_TIME is '更新时间';
-
-comment on column RESOURCE_ALBUM.RESOURCE_ID is '资源ID';
-
-comment on column RESOURCE_ALBUM.ALBUM_ID is '专辑ID';
-
-create table RESOURCE_TYPE
-(
-  ID CHARACTER VARYING(19) not null
-    primary key,
-  RESOURCE_ID CHARACTER VARYING(19) not null,
-  TYPE_ID CHARACTER VARYING(19) not null,
-  CREATE_TIME TIMESTAMP not null,
-  UPDATE_TIME TIMESTAMP
-);
-
-comment on table RESOURCE_TYPE is '资源和类型关联';
-
-comment on column RESOURCE_TYPE.ID is 'ID';
-
-comment on column RESOURCE_TYPE.RESOURCE_ID is '资源id';
-
-comment on column RESOURCE_TYPE.TYPE_ID is '类型id';
-
-comment on column RESOURCE_TYPE.CREATE_TIME is '创建时间';
-
-comment on column RESOURCE_TYPE.UPDATE_TIME is '更新时间';
-
-create table RESOURCE_TYPE_MAP
-(
-  ID CHARACTER VARYING(19) not null
-    primary key,
-  PARENT_ID CHARACTER VARYING(19),
-  NAME CHARACTER VARYING(90) not null,
-  CREATE_TIME TIMESTAMP not null,
-  UPDATE_TIME TIMESTAMP
-);
-
-comment on table RESOURCE_TYPE_MAP is '资源类型映射';
-
-comment on column RESOURCE_TYPE_MAP.ID is '主键';
-
-comment on column RESOURCE_TYPE_MAP.PARENT_ID is '父类型';
-
-comment on column RESOURCE_TYPE_MAP.NAME is '名称';
-
-comment on column RESOURCE_TYPE_MAP.CREATE_TIME is '创建时间';
-
-comment on column RESOURCE_TYPE_MAP.UPDATE_TIME is '更新时间';
-
-create index RESOURCE_TYPE_MAP_1
-  on RESOURCE_TYPE_MAP (CREATE_TIME desc);
-
-create table SITE
-(
-  ID CHARACTER VARYING(19) not null
-    primary key,
-  SITE_NAME CHARACTER VARYING(90) not null,
-  URL CHARACTER VARYING(90) not null,
-  CREATE_TIME TIMESTAMP not null,
-  UPDATE_TIME TIMESTAMP,
-  DELETE_STATUS INTEGER default 0 not null
-);
-
-comment on table SITE is '站点';
-
-comment on column SITE.ID is 'ID';
-
-comment on column SITE.SITE_NAME is '网站名称';
-
-comment on column SITE.URL is '网站地址';
-
-comment on column SITE.CREATE_TIME is '创建时间';
-
-comment on column SITE.UPDATE_TIME is '更细时间';
-
-comment on column SITE.DELETE_STATUS is '删除状态0未删除1删除';
-
-create index SITE_1
-  on SITE (SITE_NAME);
-
-create index SITE_2
-  on SITE (CREATE_TIME);
-
-create index SITE_3
-  on SITE (UPDATE_TIME);
+create index MEDIA.SITE_3
+  on MEDIA.SITE (UPDATE_TIME);
 
--- ddl/update/drop.sql
+++ ddl/update/drop.sql
@@ -5,3 +5,8 @@ drop table public.RESOURCE_ALBUM;
 drop table public.RESOURCE_TYPE;
 drop table public.RESOURCE_TYPE_MAP;
 drop table public.SITE;
+
+drop table media.album;
+drop table media.RESOURCE_ALBUM;
+drop table media.RESOURCE_TYPE;
+drop table media.RESOURCE_TYPE_MAP;
