07a5240a921fc8210257a90b7658b993ae91a5c6
wrote-code <developer@noreply.email.com>
2023-05-23 00:54:08 +0800

    简化H2Server逻辑

--- application/src/main/java/com/sheepfly/media/H2Server.java
+++ application/src/main/java/com/sheepfly/media/H2Server.java
@@ -2,10 +2,8 @@ package com.sheepfly.media;
 
 import cn.hutool.core.io.resource.ResourceUtil;
 import com.alibaba.druid.pool.DruidDataSource;
+import org.h2.tools.Server;
 
-import java.sql.Connection;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -23,10 +21,6 @@ public class H2Server {
         dataSource.setPassword(properties.getProperty("spring.datasource.druid.password"));
         dataSource.setDriverClassName(properties.getProperty("spring.datasource.druid.driver-class-name"));
         dataSource.init();
-        List<Connection> list = new ArrayList<>(10);
-        for (int i = 0; i < 10; i++) {
-            list.add(dataSource.getConnection());
-        }
-        dataSource.getActiveCount();
+        Server.startWebServer(dataSource.getConnection());
     }
 }
