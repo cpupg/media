2a571705222aa5bd1315f5400e0ed63f5f0d521f
wrote-code <developer@noreply.email.com>
2024-02-05 02:18:24 +0800

    错误代码首字母统一改成E

--- common/src/main/java/com/sheepfly/media/common/exception/ErrorCode.java
+++ common/src/main/java/com/sheepfly/media/common/exception/ErrorCode.java
@@ -13,19 +13,8 @@ package com.sheepfly.media.common.exception;
  * <ol>
  * <li>8位错误码都是0：表示操作成功。</li>
  * <li>错误码3-5位小于200，表示和模块无关的通用错误。200开始表示业务错误。</li>
- * <li>Z0999999：表示未知错误，此错误码由全局异常处理器抛出。</li>
+ * <li>E0999999：表示未知错误，此错误码由全局异常处理器抛出。</li>
  * </ol>
- *
- * <p>目前的错误类型有以下几种：</p>
- * <ul>
- * <li>A:用户填写的表单错误。</li>
- * <li>B:表单校验错误。</li>
- * <li>C:业务异常。大多数异常都可以作为业务异常。</li>
- * <li>D:代码中经过判断发现的异常，比如判断某个对象是否为空。</li>
- * <li>E:数据库异常。</li>
- * <li>F:文件操作异常。</li>
- * <li>Z:未知错误，通常是没有捕获的异常。</li>
- * </ul>
  *
  * <p>模块代码如下：</p>
  * <ul>
@@ -53,11 +42,11 @@ public enum ErrorCode {
     /**
      * 操作成功。
      */
-    OPERATION_SUCCESS("00000000", "操作成功"),
+    OPERATION_SUCCESS("E0000000", "操作成功"),
     /**
      * 用户未登录。
      */
-    USER_NOT_LOG_IN("C0100001", "用户未登录"),
+    USER_NOT_LOG_IN("E0100001", "用户未登录"),
 
     /*
      ***************************************************************************
@@ -66,13 +55,13 @@ public enum ErrorCode {
     /**
      * 网站名称和网站地址不能为空。
      */
-    SITE_NAME_URL_CANT_BE_NULL("C0200001", "网站名称和网站地址不能为空"),
+    SITE_NAME_URL_CANT_BE_NULL("E0200001", "网站名称和网站地址不能为空"),
     /**
      * 网站地址不合法。
      */
-    URL_IS_ILLEGAL("C0200002", "网站地址不合法"),
-    SITE_ID_CANT_NULL("C0200003", "网站标识不能为空"),
-    SITE_CANT_BE_DELETE("C0200004", "网站下有作者，不能删除网站"),
+    URL_IS_ILLEGAL("E0200002", "网站地址不合法"),
+    SITE_ID_CANT_NULL("E0200003", "网站标识不能为空"),
+    SITE_CANT_BE_DELETE("E0200004", "网站下有作者，不能删除网站"),
     /*
      * 网站模块结束
      ***************************************************************************
@@ -85,16 +74,16 @@ public enum ErrorCode {
     /**
      * 注册网站为空。
      */
-    AUTHOR_SITE_CANT_BE_NULL("C0201001", "注册网站不能为空"),
+    AUTHOR_SITE_CANT_BE_NULL("E0201001", "注册网站不能为空"),
     /**
      * 用户名和用户id不能同时为空。
      */
-    AUTHOR_ID_AND_NAME_CANT_NULL("C0201002", "用户名和用户id不能同时为空"),
+    AUTHOR_ID_AND_NAME_CANT_NULL("E0201002", "用户名和用户id不能同时为空"),
     /**
      * 作者标识不能为空。
      */
-    AUTHOR_ID_CANT_BE_NULL("C0201002", "作者标识不能为空"),
-    AUTHOR_ASSOCIATE_RESOURCE("C0201003", "作者下由关联的资源"),
+    AUTHOR_ID_CANT_BE_NULL("E0201002", "作者标识不能为空"),
+    AUTHOR_ASSOCIATE_RESOURCE("E0201003", "作者下由关联的资源"),
     /*
      * 作者模块结束
      ***************************************************************************
@@ -104,11 +93,11 @@ public enum ErrorCode {
      ***************************************************************************
      * 模块[资源][res]开始[202]
      */
-    RESOURCE_MKDIR_FAIL("D0202001", "创建目录失败，请重试"),
-    RES_ADD_FAIL_BY_DUPLICATED("D0202002", "资源重复，添加失败"),
-    RES_TAG_NAME_CANT_NULL("D0202003", "标签名称不能为空"),
-    RES_TAG_NOT_FOUND("D0202004", "标签不存在"),
-    RES_DONT_HAVE_THIS_TAG("D0202005", "指定资源没有设置此标签"),
+    RESOURCE_MKDIR_FAIL("E0202001", "创建目录失败，请重试"),
+    RES_ADD_FAIL_BY_DUPLICATED("E0202002", "资源重复，添加失败"),
+    RES_TAG_NAME_CANT_NULL("E0202003", "标签名称不能为空"),
+    RES_TAG_NOT_FOUND("E0202004", "标签不存在"),
+    RES_DONT_HAVE_THIS_TAG("E0202005", "指定资源没有设置此标签"),
     /*
      * 模块[资源][res]结束[203]
      ***************************************************************************
@@ -118,9 +107,9 @@ public enum ErrorCode {
      ***************************************************************************
      * 模块[目录][DIRECTORY]开始[203]
      */
-    DIRECTORY_MUST_BE_ABSOLUTE("D0203001", "目录格式有错误，必须是绝对路径"),
-    DIRECTORY_ILLEGAL_DRIVER("D0203002", "目录格式错误，不正确的Windows盘符"),
-    DIRECTORY_EMPTY_ROOT_DIRECTORY("C0203003", "根目录不存在，请检查数据库"),
+    DIRECTORY_MUST_BE_ABSOLUTE("E0203001", "目录格式有错误，必须是绝对路径"),
+    DIRECTORY_ILLEGAL_DRIVER("E0203002", "目录格式错误，不正确的Windows盘符"),
+    DIRECTORY_EMPTY_ROOT_DIRECTORY("E0203003", "根目录不存在，请检查数据库"),
     /*
      * 模块[目录][DIRECTORY]结束[203]
      ***************************************************************************
@@ -129,10 +118,10 @@ public enum ErrorCode {
      ***************************************************************************
      * 模块[标签][tag]开始[204]
      */
-    TAG_NAME_CANT_BE_EMPTY("C0204001",  "标签名不能为空"),
-    TAG_NAME_TOO_LONG("D0204001","标签名太长"),
-    TAG_RES_ID_CANT_BE_NULL("D0204002","缺少资源标识"),
-    TAG_RES_ID_AND_TAG_ID_CANT_BE_NULL("D0204003","资源标识或标签标识为空"),
+    TAG_NAME_CANT_BE_EMPTY("E0204001", "标签名不能为空"),
+    TAG_NAME_TOO_LONG("E0204001", "标签名太长"),
+    TAG_RES_ID_CANT_BE_NULL("E0204002", "缺少资源标识"),
+    TAG_RES_ID_AND_TAG_ID_CANT_BE_NULL("E0204003", "资源标识或标签标识为空"),
     /*
      * 模块[标签][tag]结束[204]
      ***************************************************************************
@@ -146,38 +135,38 @@ public enum ErrorCode {
     /**
      * 要删除的数据不存在。
      */
-    DELETE_NOT_EXIST_DATA("C0101001", "要删除的数据不存在"),
+    DELETE_NOT_EXIST_DATA("E0101001", "要删除的数据不存在"),
     /**
      * 重复的数据。
      */
-    SAVE_DUPLICATED_DATA("C0101002", "重复的数据"),
+    SAVE_DUPLICATED_DATA("E0101002", "重复的数据"),
     /**
      * 覆盖文件。
      */
-    REWRITE_FILE("C0101003", "覆盖文件"),
+    REWRITE_FILE("E0101003", "覆盖文件"),
     /**
      * 文件不存在。
      */
-    FILE_NOT_FOUND("C0101004", "文件不存在"),
+    FILE_NOT_FOUND("E0101004", "文件不存在"),
     /**
      * 转换失败。
      */
-    DATA_TO_FORM_FAIL("C0101005", "转换失败"),
+    DATA_TO_FORM_FAIL("E0101005", "转换失败"),
     /**
      * 缺少请求参数。
      */
-    REQUEST_VALUE_IS_LOST("C0101006", "缺少请求参数"),
-    PAGINATION_ERROR("C0101007", "缺少分页参数"),
+    REQUEST_VALUE_IS_LOST("E0101006", "缺少请求参数"),
+    PAGINATION_ERROR("E0101007", "缺少分页参数"),
     /**
      * 逻辑删除失败：调用反射创建实例失败。
      */
-    LOGIC_DELETE_CREATE_FAIL("Z0103001", "逻辑删除失败：创建实例失败"),
-    LOGIC_DELETE_NOT_SUPPORT("Z0103002", "目标实例不支持逻辑删除"),
+    LOGIC_DELETE_CREATE_FAIL("E0103001", "逻辑删除失败：创建实例失败"),
+    LOGIC_DELETE_NOT_SUPPORT("E0103002", "目标实例不支持逻辑删除"),
     /**
      * 未知错误，请联系管理员。
      */
-    UNEXPECT_ERROR("Z0999999", "未知错误，请联系管理员"),
-    VALIDATE_ERROR("Z0999998", "输入参数有误");
+    UNEXPECT_ERROR("E0999999", "未知错误，请联系管理员"),
+    VALIDATE_ERROR("E0999998", "输入参数有误");
     /* 公共模块结束
      **************************************************************************
      */
