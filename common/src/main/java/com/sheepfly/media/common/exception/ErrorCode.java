ed666667dc34a1b22e226bd71565b06b46814887
wrote-code <developer@noreply.email.com>
2024-02-19 01:24:58 +0800

    增加请求getFile，用来返回文件

--- common/src/main/java/com/sheepfly/media/common/exception/ErrorCode.java
+++ common/src/main/java/com/sheepfly/media/common/exception/ErrorCode.java
@@ -146,6 +146,7 @@ public enum ErrorCode {
      */
     FILE_EMPTY_BUSINESS_CODE_TYPE("E0206001", "缺少业务代码和业务类型"),
     FILE_EMPTY_BUSINESS_CODE("E0206002", "缺少业务代码"),
+    FILE_EMPTY_FILENAME("E0206003", "文件名不能为空"),
     /*
      * 模块[文件][file]结束[300]
      ***************************************************************************
--- web/src/main/java/com/sheepfly/media/web/controller/FileController.java
+++ web/src/main/java/com/sheepfly/media/web/controller/FileController.java
@@ -14,6 +14,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +22,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -85,5 +88,14 @@ public class FileController {
             throw new BusinessException(ErrorCode.FILE_EMPTY_BUSINESS_CODE);
         }
         return service.queryFileList(businessCode);
+    }
+
+    @GetMapping("/getFile")
+    public void getFile(@RequestParam("filename") String filename, HttpServletResponse response) throws IOException {
+        try (InputStream is = new FileInputStream("data/file/file_dir/" + filename);
+             OutputStream os = response.getOutputStream()) {
+            byte[] bytes = IOUtils.readFully(is, is.available());
+            os.write(bytes);
+        }
     }
 }
