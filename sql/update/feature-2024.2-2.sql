ce12cffb28ab260122bf37213ccf1da56d6b74ef
wrote-code <developer@noreply.email.com>
2024-02-19 16:30:34 +0800

    fileUpload表增加扩展名列

--- sql/update/feature-2024.2-2.sql
+++ sql/update/feature-2024.2-2.sql
@@ -1,3 +1,4 @@
+drop table media.FILE_UPLOAD;
 create table media.file_upload
 (
   id varchar(19) not null primary key,
@@ -5,6 +6,7 @@ create table media.file_upload
   filename varchar(32) not null default '',
   business_code varchar(19) not null default '0',
   business_type tinyint not null default 0,
+  extension varchar(8) not null default '',
   upload_status tinyint not null default 0,
   delete_status tinyint not null default 0,
   upload_time timestamp not null default current_timestamp,
@@ -19,6 +21,7 @@ comment on column media.file_upload.original_filename is '原始文件名';
 comment on column media.file_upload.filename is '保存时的文件名';
 comment on column media.file_upload.business_code is '业务代码，用来关联业务';
 comment on column media.file_upload.business_type is '业务类型，用来区分业务';
+comment on column media.file_upload.extension is '文件扩展名';
 comment on column media.file_upload.upload_status is '上传状态1开始上传2上传完成';
 comment on column media.file_upload.delete_status is '删除状态';
 comment on column media.file_upload.upload_time is '上传时间';
