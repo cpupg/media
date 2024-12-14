06c747cb5f4d829ea8f03884505893b963ba2c57
chen <39870481+cpupg@users.noreply.github.com>
2024-12-15 01:53:57 +0800

    增加sonar扫描脚本

---
+++ sonar.bat
@@ -0,0 +1,1 @@
+mvn -DskipTests=true compile sonar:sonar -Dsonar.projectKey=media -Dsonar.projectName='media' -Dsonar.host.url=http://localhost:9000 -Dsonar.token=sqp_8b0d3723e9b14e780da1363acaebe73feecc19ff
