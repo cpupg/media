7415a50131bbdbab28e07cd5cc39a269c42d2039
chen <39870481+cpupg@users.noreply.github.com>
2025-01-19 01:05:30 +0800

    增加getRate请求

--- web/src/main/java/com/sheepfly/media/web/controller/RateController.java
+++ web/src/main/java/com/sheepfly/media/web/controller/RateController.java
@@ -2,15 +2,19 @@ package com.sheepfly.media.web.controller;
 
 import com.sheepfly.media.common.exception.BusinessException;
 import com.sheepfly.media.common.exception.ErrorCode;
+import com.sheepfly.media.common.http.ResponseData;
 import com.sheepfly.media.common.vo.RateVo;
+import com.sheepfly.media.dataaccess.entity.Rate;
 import com.sheepfly.media.service.base.IResourceService;
 import com.sheepfly.media.service.base.RateService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Optional;
 
 /**
  * 评分请求。
@@ -34,7 +38,7 @@ public class RateController {
      * @return 评分对象。
      */
     @PostMapping("/rate")
-    public RateVo rate(@RequestParam("resourceId") String resourceId, @RequestParam("rate") int rate) {
+    public ResponseData<RateVo> rate(@RequestParam("resourceId") String resourceId, @RequestParam("rate") int rate) {
         if (rate < 0 || rate > 10) {
             throw new BusinessException(ErrorCode.RATE_RANGE_NOT_ALLOW);
         }
@@ -42,6 +46,25 @@ public class RateController {
         if (res == null) {
             throw new BusinessException(ErrorCode.RES_NOT_FOUND);
         }
-        return rateService.rateResource(resourceId, rate);
+        RateVo rateVo = rateService.rateResource(resourceId, rate);
+        return ResponseData.success(rateVo);
+    }
+
+    /**
+     * 获取资源的评分。
+     *
+     * @param resourceId
+     *
+     * @return 评分。
+     */
+    @PostMapping("/getRate")
+    public ResponseData<RateVo> getRate(@RequestParam("resourceId") String resourceId) {
+        Optional<Rate> one = rateService.findOne((r, q, b) -> b.equal(r.get("resourceId"), resourceId));
+        if (one.isPresent()) {
+            RateVo rateVo = new RateVo();
+            BeanUtils.copyProperties(one.get(), rateVo);
+            return ResponseData.success(rateVo);
+        }
+        return ResponseData.success(null);
     }
 }
